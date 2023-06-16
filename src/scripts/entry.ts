436d27b9c4f9cd69b8af4c0e1ba1b5c5e324c4cf
spoon-inoue <h-inoue@studio-spoon.co.jp>
2023-06-16 16:55:35 +0900

    ios safariもaudio trackを消した動画ならWebCodecsAPIに対応している

--- src/scripts/entry.ts
+++ src/scripts/entry.ts
@@ -1,39 +1,10 @@
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { isSp, matchSp, resolvePath } from './utils'
+import { resolvePath } from './utils'
 
-gsap.registerPlugin(ScrollTrigger)
-const video = document.querySelector<HTMLVideoElement>('.video-container .video')!
-let tl: gsap.core.Timeline
+const { ScrollyVideo } = window as any
 
-if (isSp()) {
-  // SPの場合は、gsapでcurrentTimeを更新する
-  tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: '.video-container',
-      start: 'top top',
-      end: 'bottom bottom',
-      scrub: true,
-    },
-  })
-
-  video.addEventListener('loadedmetadata', () => {
-    tl.fromTo(video, { currentTime: 0 }, { currentTime: video?.duration })
-  })
-} else {
-  // PCの場合は、ScrollyVideoを使う
-  const { ScrollyVideo } = window as any
-
-  new ScrollyVideo({
-    scrollyVideoContainer: 'scrolly-video',
-    src: video.src,
-    cover: true,
-    full: true,
-  })
-}
-
-matchSp().onchange = () => {
-  tl?.scrollTrigger?.kill()
-  tl?.kill()
-  location.reload()
-}
+new ScrollyVideo({
+  scrollyVideoContainer: 'scrolly-video',
+  src: resolvePath('videos/9mb_13s.mp4'),
+  cover: true,
+  full: true,
+})
