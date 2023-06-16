3f26998119b76253351d1da7f0dd17745572aa85
spoon-inoue <h-inoue@studio-spoon.co.jp>
2023-06-16 11:44:58 +0900

    init

--- src/scripts/entry.ts
+++ src/scripts/entry.ts
@@ -4,8 +4,8 @@ const ScrollyVideo = (window as any).ScrollyVideo
 
 new ScrollyVideo({
   scrollyVideoContainer: 'scrolly-video',
-  src: resolvePath('/videos/2.99mb_10s.mp4'),
-  // src: resolvePath('/videos/7.37mb_20s.mp4'),
+  // src: resolvePath('/videos/2.99mb_10s.mp4'),
+  src: resolvePath('/videos/7.37mb_20s.mp4'),
   cover: true,
   full: true,
 })
