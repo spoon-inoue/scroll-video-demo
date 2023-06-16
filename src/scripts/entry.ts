f2577157d3f850a290e83e8dfd1df4d9f33094c5
spoon-inoue <h-inoue@studio-spoon.co.jp>
2023-06-16 18:17:58 +0900

    another video

--- src/scripts/entry.ts
+++ src/scripts/entry.ts
@@ -1,10 +1,21 @@
+import Lenis from '@studio-freight/lenis'
 import { resolvePath } from './utils'
 
 const { ScrollyVideo } = window as any
 
 new ScrollyVideo({
   scrollyVideoContainer: 'scrolly-video',
-  src: resolvePath('videos/9mb_13s.mp4'),
-  cover: true,
-  full: true,
+  // src: resolvePath('videos/9mb_13s.mp4'),
+  src: resolvePath('videos/7mb_20s.mp4'),
+  // playbackRate: 20,
+  frameThreshold: 0.001,
 })
+
+const lenis = new Lenis()
+
+function raf(time: number) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
